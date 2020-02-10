5c25bfc24cabb89e6891586838cc9fb2aa89d22c
Hugo Bock <h.bock@easyrecrue.com>
2020-02-10 15:40:59 +0100

    routin and menu

---
+++ src/features/home/home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { StyledHome } from "./styled-home";
+
+const Home = () => {
+  return (
+    <StyledHome>
+      <div>
+        <div className="menu">
+          <Link className="menu-item" to="/">
+            <p>Home</p>
+          </Link>
+          <Link className="menu-item" to="/analytics">
+            <p>Analytic Board</p>
+          </Link>
+          <Link className="menu-item" to="/clock">
+            <p>Clock</p>
+          </Link>
+        </div>
+      </div>
+    </StyledHome>
+  );
+};
+
+export default Home;
---
+++ src/features/home/styled-home.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+  height: 100vh;
+
+  * {
+    text-decoration: none;
+    color: #f6a61e;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f6a61e;
+
+  .menu {
+    display: flex;
+    flex-wrap: wrap;
+    width: fit-content;
+    justify-content: space-around;
+    align-items: center;
+    max-width: 260px;
+    align-self: center;
+    border-radius: 14px;
+    padding: 10px;
+  }
+
+  .menu-item {
+    display: flex;
+    justify-content: center;
+
+    width: 100px;
+    height: 100px;
+    padding: 10px 30px;
+    border-radius: 14px;
+    margin: 10px;
+    background-color: #443b8f;
+    box-shadow: 10px 4px 30px rgba(0, 0, 0, 0.4);
+
+    transition: box-shadow 0.2s, transform 0.2s;
+
+    :hover {
+      box-shadow: 15px 7px 40px rgba(0, 0, 0, 0.7);
+      transform: scale(1.1);
+    }
+
+    & > * {
+      align-self: center;
+      text-align: center;
+    }
+  }
+`;
