335a6b6d7a2ecd70d0016f6f445f13133dbbe7fe
Hugo Bock <h.bock@easyrecrue.com>
2020-02-13 15:35:42 +0100

    clean

--- src/features/post-component/post-component.js
+++ src/features/post-component/post-component.js
@@ -22,9 +22,6 @@ const PostComponent = ({ postId }) => {
   ];
   const iconId =
     state.iconId === undefined ? 0 : state.iconId % businessIcons.length;
-  // console.log(state.iconId);
-  // console.log(iconId);
-  // console.log("../../assets/images/forum/" + businessIcons[iconId] + ".png");
   for (let i = 0; i < businessIcons.length; i++) {
     require("../../assets/images/forum/" + businessIcons[i] + ".png");
   }
