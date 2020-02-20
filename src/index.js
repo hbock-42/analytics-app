b523398b32774affc7d5c3f28bfbba4015070133
Hugo Bock <h.bock@easyrecrue.com>
2020-02-20 16:18:37 +0100

    start adding redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import AnalyticBoard from "./features/analytic-bord/analytic-board";
-import Clock from "./features/clock/clock";
-import Home from "./features/home/home";
-import Posts from "./features/posts/posts.js";
-import MenuShortcut from "./features/menu-shortcut/menu-shortcut";
-import { GlobalStyle } from "./features/app/global-style";
-import * as serviceWorker from "./serviceWorker";
-
-const routing = (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/analytics" component={AnalyticBoard} />
-      <Route path="/clock" component={Clock} />
-      <Route path="/posts" component={Posts} />
-      <Route component={Home} />
-    </Switch>
-    <GlobalStyle />
-    <MenuShortcut />
-  </Router>
-);
-
+import routing from "./router";
+import StoreProvider from "./stores/store";
 ReactDOM.render(routing, document.getElementById("root"));
